6f27e2bb1ab388f63ce522b932f0b5df2c4657c9
Arnau <arnau_mtez@hotmail.com>
2019-09-26 20:40:49 +0200

    HTTP Server initializacion.
    We need to redefine the model and controller to adjust it to out requirements

---
+++ LaprasDB/src/main/java/me/laprasdb/MainAplicationClass.java
@@ -0,0 +1,14 @@
+package me.laprasdb;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainAplicationClass {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MainAplicationClass.class, args);
+    }
+
+}
