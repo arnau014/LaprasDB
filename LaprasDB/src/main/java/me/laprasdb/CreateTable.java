3b360c75726d74a3f4e0d289c2580742813ba662
arnau <arnau_mtez@hotmail.com>
2019-10-02 23:17:19 +0200

    Create file and write on it

--- LaprasDB/src/main/java/me/laprasdb/CreateTable.java
+++ LaprasDB/src/main/java/me/laprasdb/CreateTable.java
@@ -1,6 +1,6 @@
 package me.laprasdb;
 
-import java.lang.reflect.Array;
+import java.io.*;
 import java.util.*;
 
 /**
@@ -18,15 +18,39 @@ public class CreateTable {
         this.content = content;
         this.table = new HashMap();
 
-        newTable(content, table);
+        try {
+            newTable(content, table);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     // Placeholder method to create the file
-    public void newTable(String[] name, Map<String, Map<String, Object>> table){
-        System.out.println("Arribo aqui amb el nom");
-        System.out.println(name[0]);
+    public void newTable(String[] name, Map<String, Map<String, Object>> table) throws IOException {
+        File f = new File(name[0]);
 
+        //if file does not exist, we create it
+        if(!f.isFile()){
+            try (Writer writer = new BufferedWriter(new OutputStreamWriter(
+                    new FileOutputStream(name[0]), "utf-8"))) {
+            }
+        }
 
+        BufferedReader reader = new BufferedReader(new FileReader(name[0]));
+        int lines = 0;
+        while (reader.readLine() != null) lines++;
+        reader.close();
+
+        FileOutputStream fos =new FileOutputStream(f,true);
+        Writer csvWriter = new BufferedWriter(new OutputStreamWriter(fos));
+
+        for (int i = 1; i < name.length; ++i ) {
+            csvWriter.write(String.valueOf(lines++));
+            csvWriter.write(',');
+            csvWriter.write(name[i]);
+            csvWriter.write("\n");
+        }
+        csvWriter.close();
 
     }
 
