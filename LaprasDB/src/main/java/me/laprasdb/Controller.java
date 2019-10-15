7f38c2099892d727d48ed9413d24b431624702ce
xaviRodri <xavier.rodriguez@betika.com>
2019-10-15 16:43:39 +0200

    CreateTable refactor & Create tolerant to existant tables

--- LaprasDB/src/main/java/me/laprasdb/Controller.java
+++ LaprasDB/src/main/java/me/laprasdb/Controller.java
@@ -1,10 +1,10 @@
 package me.laprasdb;
 
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Map;
@@ -13,10 +13,21 @@ import java.util.Map;
 public class Controller {
 
     @RequestMapping(value = "/create", method = RequestMethod.POST)
-    public CreateTable create(@RequestBody Map<String, Object> table ) throws IOException {
+    public ResponseEntity create(@RequestBody Map<String, Object> table ) throws IOException {
         String tablename = table.get("tablename").toString();
         ArrayList<String> columns = (ArrayList<String>) table.get("columns");
-        return new CreateTable(tablename, columns);
+
+        // Check if file exists
+        File tempFile = new File("tables/" + tablename + ".json");
+
+        // If the file exists, return CONFLICT ERROR
+        if(tempFile.exists()){
+            return new ResponseEntity(HttpStatus.CONFLICT);
+        }
+
+        // If doesn't exist, create the new table
+        new Table(tablename, columns);
+        return new ResponseEntity(HttpStatus.CREATED);
     }
 
 
