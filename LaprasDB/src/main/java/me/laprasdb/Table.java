37266f6d2b82743762bb553c6f0742b6bd35ddf5
xaviRodri <xavier.rodriguez@betika.com>
2019-12-05 19:40:05 +0100

    Added insert function
    To perform an insert, do the following:
    
    HTTP Request (POST) to http://localhost:8080/insert
    
    Body:
    {
    "tableName": "table",
    "attrs": {
    	"col1": "val1",
    	"col3": "val3"
    }
    }
    
    If the info sended is valid, the request will return a 201 (CREATED) HTTP Status.
    If not, it will return a 400 (BAD_REQUEST) HTTP Status.

--- LaprasDB/src/main/java/me/laprasdb/Table.java
+++ LaprasDB/src/main/java/me/laprasdb/Table.java
@@ -1,14 +1,24 @@
 package me.laprasdb;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
 import java.io.*;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
 
 public class Table {
+
+    private String tableName;
+
+    public Table(String tableName) throws IOException {
+        this.tableName = tableName;
+    }
 
     public Table(String tableName, ArrayList<String> columnNames) throws IOException {
         //create ObjectMapper instance
@@ -39,5 +49,46 @@ public class Table {
 
         //write Map object to json file
         objectMapper.writeValue(new File("tables/" + tableName + "/" + tableName + ".json"), table);
+    }
+
+    public boolean insert(Map<String, Object> attrs) {
+        //create ObjectMapper instance
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        //Check if all columns exist
+        File tempFile;
+
+        for ( String key : attrs.keySet() ) {
+            tempFile = new File("tables/" + this.tableName + "/" + this.tableName + "_" + key + ".json");
+
+            // If the file exists, return CONFLICT ERROR
+            if(!tempFile.exists()){
+                return false;
+            }
+        }
+
+        //Get last id
+        try {
+            File tableFile = new File("tables/" + tableName + "/" + tableName + ".json");
+            Map<String, Object> tableInfo =  objectMapper.readValue(tableFile, HashMap.class);
+            int lastId = Integer.parseInt(tableInfo.get("lastId").toString());
+
+            for ( String key : attrs.keySet() ) {
+                HashMap<Integer,Object> column = objectMapper.readValue(new File("tables/" + this.tableName + "/" + this.tableName + "_" + key + ".json"), HashMap.class);
+                // Write to each column the new value with the lastId as the key
+                column.put(lastId, attrs.get(key));
+
+                // Write again the column to the file
+                objectMapper.writeValue(new File("tables/" + this.tableName + "/" + this.tableName + "_" + key + ".json"), column);
+            }
+
+            // Update the lastId
+            tableInfo.put("lastId", lastId+1);
+            objectMapper.writeValue(tableFile, tableInfo);
+        } catch (IOException e) {
+            return false;
+        }
+
+        return true;
     }
 }
